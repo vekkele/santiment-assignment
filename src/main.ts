85fb578deb27327abb628442c8228e325ca3aa30
Vlad Keliy <vekkele@gmail.com>
2023-06-14 19:59:50 +0500

    🔧 Make initial config that works with san-webkit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import './app.css'
+import 'san-webkit/src/styles/main.css'
 import App from './App.svelte'
+import Dialogs from 'webkit/ui/Dialog/Dialogs.svelte'
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: document.getElementById('app') as Element,
 })
+
+const ROOT_CTX = app.$$.context
+
+new Dialogs({ target: document.body, context: ROOT_CTX })
 
 export default app
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
+
+process.env.GQL_SERVER_URL = 'https://api-stage.santiment.net/graphql'
+process.env.IS_DEV_MODE = (process.env.NODE_ENV !== 'production').toString()
+process.env.MEDIA_PATH = '/node_modules/san-webkit/lib'
+process.env.ICONS_PATH = '/node_modules/san-webkit/lib/icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: 'webkit', replacement: path.join(__dirname, 'node_modules/san-webkit/lib/') },
+      { find: 'san-webkit', replacement: path.join(__dirname, 'node_modules/san-webkit/') },
+    ],
+  },
+  define: {
+    'process.browser': true,
+    'process.env': process.env,
+    'process.env.IS_DEV_MODE': process.env.IS_DEV_MODE,
+  },
 })
